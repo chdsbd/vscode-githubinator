a5c9adf192a152b9a4b8b1e7dba6364c42875b88
Christopher Dignam <chris@dignam.xyz>
2019-03-01 18:25:40 -0500

    Fix globbing for prettier to include top level src files

--- src/providers.ts
+++ src/providers.ts
@@ -1,56 +1,56 @@
-import * as path from "path";
-import * as url from "url";
-import { IGithubinatorConfig } from "./extension";
-import { cleanHostname } from "./utils";
+import * as path from "path"
+import * as url from "url"
+import { IGithubinatorConfig } from "./extension"
+import { cleanHostname } from "./utils"
 
 interface IGetUrl {
-  selection: [number, number];
-  head: string;
-  mode: "blob" | "blame";
-  relativeFilePath: string;
-  origin: string;
-  providersConfig: IGithubinatorConfig["providers"];
+  selection: [number, number]
+  head: string
+  mode: "blob" | "blame"
+  relativeFilePath: string
+  origin: string
+  providersConfig: IGithubinatorConfig["providers"]
 }
 
 interface IProvider {
-  getUrl(params: IGetUrl): { fileUrl: string; repoUrl: string } | null;
+  getUrl(params: IGetUrl): { fileUrl: string; repoUrl: string } | null
 }
 
 export class Github implements IProvider {
-  SSH = /^git@github\.com:(.*)\/(.*)\.git$/;
-  HTTPS = /^https:\/\/github\.com\/(.*)\/(.*)\.git$/;
-  HOSTNAME = "github.com";
+  SSH = /^git@github\.com:(.*)\/(.*)\.git$/
+  HTTPS = /^https:\/\/github\.com\/(.*)\/(.*)\.git$/
+  HOSTNAME = "github.com"
   getUrl({
     selection,
     relativeFilePath,
     head,
     mode,
     providersConfig,
-    origin
+    origin,
   }: IGetUrl): { fileUrl: string; repoUrl: string } | null {
-    const [start, end] = selection;
-    const ssh = origin.match(this.SSH);
-    const https = origin.match(this.HTTPS);
+    const [start, end] = selection
+    const ssh = origin.match(this.SSH)
+    const https = origin.match(this.HTTPS)
     let [_, org, repo] =
-      ssh != null ? ssh : https != null ? https : [null, null, null];
+      ssh != null ? ssh : https != null ? https : [null, null, null]
     if (org == null || repo == null) {
-      return null;
+      return null
     }
-    const providerHostname = providersConfig["github"];
+    const providerHostname = providersConfig["github"]
     const hostname = cleanHostname(
-      providerHostname ? providerHostname : this.HOSTNAME
-    );
-    const rootUrl = `https://${hostname}/`;
+      providerHostname ? providerHostname : this.HOSTNAME,
+    )
+    const rootUrl = `https://${hostname}/`
     // Github uses 1-based indexing
-    const lines = `L${start + 1}-L${end + 1}`;
-    const repoUrl = new url.URL(path.join(org, repo), rootUrl);
+    const lines = `L${start + 1}-L${end + 1}`
+    const repoUrl = new url.URL(path.join(org, repo), rootUrl)
     const parsedUrl = new url.URL(
       path.join(org, repo, mode, head, relativeFilePath),
-      rootUrl
-    );
-    parsedUrl.hash = lines;
-    return { fileUrl: parsedUrl.toString(), repoUrl: repoUrl.toString() };
+      rootUrl,
+    )
+    parsedUrl.hash = lines
+    return { fileUrl: parsedUrl.toString(), repoUrl: repoUrl.toString() }
   }
 }
 
-export const providers = [Github];
+export const providers = [Github]
