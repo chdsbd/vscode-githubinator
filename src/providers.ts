eb2edb0e2e844412b6058fc22fbed5a0d836a92d
Christopher Dignam <chris@dignam.xyz>
2019-03-01 18:30:23 -0500

    Add Gitlab provider

--- src/providers.ts
+++ src/providers.ts
@@ -57,4 +57,43 @@ export class Github implements IProvider {
   }
 }
 
-export const providers = [Github]
+class Gitlab implements IProvider {
+  SSH = /^git@gitlab\.com:(.*)\/(.*)\.git$/
+  HTTPS = /^https:\/\/gitlab.com\/(.*)\/(.*)\.git$/
+  HOSTNAME = "gitlab.com"
+  // https://gitlab.com/my_org/my_repo/blob/master/app/main.py#L3-4
+  getUrl({
+    selection,
+    relativeFilePath,
+    head,
+    mode,
+    providersConfig,
+    origin,
+  }: IGetUrl): IUrlInfo | null {
+    const ssh = origin.match(this.SSH)
+    const https = origin.match(this.HTTPS)
+    const [_, org, repo] = ssh ? ssh : https ? https : [null, null, null]
+    if (org == null || repo == null) {
+      return null
+    }
+
+    const providerHostname = providersConfig["gitlab"]
+    const hostname = cleanHostname(
+      providerHostname ? providerHostname : this.HOSTNAME,
+    )
+    const rootUrl = `https://${hostname}/`
+
+    const [start, end] = selection
+    // The format is L34-56 (this is one character off from Github)
+    const lines = `L${start + 1}-${end + 1}`
+    const repoUrl = new url.URL(path.join(org, repo), rootUrl)
+    const parsedUrl = new url.URL(
+      path.join(org, repo, mode, head, relativeFilePath),
+      rootUrl,
+    )
+    parsedUrl.hash = lines
+    return { fileUrl: parsedUrl.toString(), repoUrl: repoUrl.toString() }
+  }
+}
+
+export const providers = [Gitlab, Github]
